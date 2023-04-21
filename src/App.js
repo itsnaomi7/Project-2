54b722ae797eb8ece9ced96993c7987bada69c5a
“[firstname <clarknaomi7@gmail.com>
2023-04-21 15:43:22 -0500

    final but I need to fix page size

--- src/App.js
+++ src/App.js
@@ -2,13 +2,50 @@ import './style.css';
 // import Player from "./component/Player";
 import Home from "./pages/Home";
 //import data from "./data";
+import React, { useEffect, useState } from "react";
 
-export default function App() {
+const App = () => {
+  const [quote, setQuote] = useState([]);
+
+  useEffect(() => {
+    
+    fetch("https://type.fit/api/quotes")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        setQuote(data);
+      });
+  }, []);
+  
+
+  const randomNum = () => Math.floor(Math.random() * quote.length);
+  const [quoteIndex, setQuoteIndex] = useState(randomNum);
+
+  const handleClick = (e) => {
+    setQuoteIndex(randomNum());
+  };
+
+  if (!quote.length) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-    <div className="homePage">
-        <Home />
+    <div className="container" id="quote-box">
+      <div className="quote-box">
+        <h1 className="quote-other" id="text">
+          {quote[quoteIndex].text}
+        </h1>
+      </div>
+      <div className="btnDiv">
+        <button id="new-quote-new" onClick={handleClick}>
+          Be Inspired
+        </button>
+        <p id="author">~{quote[quoteIndex].author || "Anon"}</p>
       </div>
     </div>
   );
-}
+};
+
+export default App;
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,66 @@
-//import React from 'react';
-// import axios from "axios";
-const btn_1 = document.getElementById('btn_1');
-const btn_2 = document.getElementById('btn_2');
-const result_1 = document.getElementById('result_1');
-const result_2 = document.getElementById('result_2');
+const form = document.querySelector("#new-quote-form");
+const input = document.querySelector("#new-quote-input");
+const list_el = document.querySelector("#quotes");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const quote = input.value;
+  console.log(quote);
+  if (!quote) {
+    alert("Say something!");
+    return;
+  }
 
 
-btn_1.addEventListener('click', Random);
-btn_2.addEventListener('click', getRandom);
+  const quote_el = document.createElement("div");
+  quote_el.classList.add("quote");
+  const quote_content_el = document.createElement("div");
+  quote_content_el.classList.add("content");
 
-function Random() {
-	fetch('https://aws.random.cat/meow')
-		.then(res => res.json())
-		.then(data => {
-			result_1.innerHTML = `<img src=${data.file} alt="cat" />`
-		});
-}
+  const quote_input_el = document.createElement("input");
+  quote_input_el.classList.add("text");
+  quote_input_el.value = quote; // so you can't edit this
+  quote_input_el.type = "text";
+  
+  quote_input_el.setAttribute("readonly", "readonly");
 
-function getRandom() {
-	fetch('https://random.dog/woof.json')
-		.then(res => res.json())
-		.then(data => {
-			if(data.url.includes('.mp4')) {
-				getRandom();
-			}
-			else {
-				result_2.innerHTML = `<img src=${data.url} alt="dog" />`;
-			}
-		});
-}
+  quote_content_el.appendChild(quote_input_el);
+
+  const quote_action_el = document.createElement("div");
+  quote_action_el.classList.add("actions");
+
+  const quote_edit_el = document.createElement("button");
+  quote_edit_el.classList.add("edit");
+  quote_edit_el.innerHTML = "Edit";
+
+  const quote_delete_el = document.createElement("button");
+  quote_delete_el.classList.add("delete");
+  quote_delete_el.innerHTML = "Delete";
+
+  quote_action_el.appendChild(quote_edit_el);
+  quote_action_el.appendChild(quote_delete_el);
+
+  quote_el.appendChild(quote_content_el);
+  quote_el.appendChild(quote_action_el);
+
+  list_el.appendChild(quote_el);
+
+  input.value = "";
+
+  quote_edit_el.addEventListener("click", () => {
+    if (quote_edit_el.innerText.toLowerCase() === "edit") {
+      quote_input_el.removeAttribute("readonly");
+      quote_input_el.focus();
+      quote_edit_el.innerText = "Save";
+    } else {
+      quote_input_el.setAttribute("readonly", "readonly");
+      quote_edit_el.innerText = "Edit";
+    }
+    console.log(list_el.childNodes);
+  });
+
+  quote_delete_el.addEventListener("click", () => {
+    list_el.removeChild(quote_el);
+  });
+});
